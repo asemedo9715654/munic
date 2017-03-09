c64f7f3f95f9d5630bcb46bf5079e9b26d0f5c26
Logan Tran <loganmartintran@icloud.com>
2017-03-09 16:47:35 -0600

    add code to have modal close when cancel button is clicked

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,6 +1,12 @@
 (function() {
     function ModalCtrl($scope, Room) {
+        $scope.createRoom = function() {
+            //function created in Room.js that adds a room to the $firebaseArray with a name according to the user input name
+        }
         
+        $scope.cancel = function() {
+            $scope.$close()
+        };
     }
     
     angular
