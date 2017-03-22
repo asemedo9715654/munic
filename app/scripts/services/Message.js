38f5d49b06f81df85a8cec4b78861ac70b97f157
Logan Tran <loganmartintran@icloud.com>
2017-03-22 17:22:05 -0500

    implement logic to add message to database, no room or username specified

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -2,17 +2,23 @@
     function Message($firebaseArray) {
         var Message = {};
         var ref = firebase.database().ref().child("messages");
+        
         Message.messages = [];
-         
+        
+        Message.currentRoom =
+        
         Message.getRoomById = function(roomName) {
             var roomRef = ref.orderByChild("roomId").equalTo(roomName);
             Message.messages = $firebaseArray(roomRef);
 
         }
         
-        Message.sendMessage = function(message) {
-            var messageRef = ref.orderByChild("content");
-            Message.messages = $firebaseArray(messageRef);
+        Message.sendMessage = function(message, roomName) {
+            var newMessage = {
+                content: message
+            }
+            
+            Message.messages.$add(newMessage);
         }
         
         return Message;
